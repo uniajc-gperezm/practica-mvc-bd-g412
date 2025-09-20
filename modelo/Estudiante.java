bc7287543d95299b9c5a659b8d4a82038bedb072
Luisa Ordoñez <luisaordonez262@gmail.com>
2025-09-19 21:00:51 -0500

    Update Estudiante.java

--- modelo/Estudiante.java
+++ modelo/Estudiante.java
@@ -2,27 +2,29 @@ package modelo;
 
 public class Estudiante {
 
-  private String nombre;
-  private int edad;
+    private String nombre;
+    private int edad;
 
-  public Estudiante(String nombre, int edad) {
-    this.nombre = nombre;
-    this.edad = edad;
-  }
+    
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 
-  public String getNombre() {
-    return nombre;
-  }
+    
+    public String getNombre() {
+        return nombre;
+    }
 
-  public void setNombre(String nombre) {
-    this.nombre = nombre;
-  }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-  public int getEdad() {
-    return edad;
-  }
+    public int getEdad() {
+        return edad;
+    }
 
-  public void setEdad(int edad) {
-    this.edad = edad;
-  }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
