d176e33f2eb69019d06c087dc6c2b6e3e1eb8042
saantiagg <santimunoz68@gmail.com>
2025-09-19 21:04:57 -0500

    Carpetas

---
+++ controlador/controladorEstudiante.java
@@ -0,0 +1,34 @@
+package controlador;
+
+import modelo.Estudiante;
+import vista.vistaEstudiante;
+
+public class controladorEstudiante {
+    private Estudiante modelo;
+    private vistaEstudiante vista;
+
+    public controladorEstudiante(Estudiante modelo, vistaEstudiante vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    public void setNombreEstudiante(String nombre) {
+        modelo.setNombre(nombre);
+    }
+
+    public String getNombreEstudiante() {
+        return modelo.getNombre();
+    }
+
+    public void setEdadEstudiante(int edad) {
+        modelo.setEdad(edad);
+    }
+
+    public int getEdadEstudiante() {
+        return modelo.getEdad();
+    }
+
+    public void actualizarVista() {
+        vista.mostrarDetallesEstudiante(modelo.getNombre(), modelo.getEdad());
+    }
+}
